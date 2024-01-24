a10592eabfcd3b4b7140e2a203e25bc7064cacd2
RodrigoTopan <rodrigo.topan.ti@gmail.com>
2024-01-23 22:28:23 -0300

    feat: add product domain

---
+++ application/product_test.go
@@ -0,0 +1,73 @@
+package application_test
+
+import (
+	"go/src/application"
+	"testing"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Price = 0
+	err = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "the price must be zero in order to have the product disabled", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.ID = uuid.NewV4().String()
+	product.Name = "hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "the status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "the price must be greater or equal zero", err.Error())
+}
+
+func Test_NewProduct(t *testing.T) {
+	product := application.NewProduct()
+	require.NotNil(t, product.ID)
+	require.Equal(t, application.DISABLED, product.Status)
+}
+
+func TestProduct_ChangePrice(t *testing.T) {
+	product := application.NewProduct()
+	const newPrice = 10.0
+	product.ChangePrice(newPrice)
+	require.Equal(t, newPrice, product.Price)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module go/src
+
+go 1.16
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/satori/go.uuid v1.2.0
+	github.com/stretchr/testify v1.8.4 // indirect
+)
